52bc38947c0ef823087be2e2cae574a1ffbc81f2
mfachrizalg <mohammad.fachrizal123@gmail.com>
2025-03-28 04:11:12 +0700

    feat: drive API

--- backend/config/drive.go
+++ backend/config/drive.go
@@ -1,1 +1,38 @@
 package config
+
+import (
+	"context"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"golang.org/x/oauth2/google"
+	"golang.org/x/oauth2/jwt"
+	"google.golang.org/api/drive/v3"
+)
+
+func ServiceAccount(secretFile string) *http.Client {
+	b, err := os.ReadFile(secretFile)
+	if err != nil {
+		log.Fatal("error while reading the credential file", err)
+	}
+	var s = struct {
+		Email      string `json:"client_email"`
+		PrivateKey string `json:"private_key"`
+	}{}
+	err = json.Unmarshal(b, &s)
+	if err != nil {
+		log.Fatal("error while unmarshaling credential file", err)
+	}
+	config := &jwt.Config{
+		Email:      s.Email,
+		PrivateKey: []byte(s.PrivateKey),
+		Scopes: []string{
+			drive.DriveScope,
+		},
+		TokenURL: google.JWTTokenURL,
+	}
+	client := config.Client(context.Background())
+	return client
+}
